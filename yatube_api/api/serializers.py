860209d09003f9b4e57043141fb9706e6af1ee55
Артём Жуков <zo.sahaal.13@gmail.com>
2022-04-12 16:59:56 +0300

    Finaly

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
 
 from posts.models import Comment, Post, Group, Follow, User
 
